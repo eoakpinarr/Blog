74d10089c5054b069b5367ce23480faa170b1f43
eoakpinarr <eoakpinar@icloud.com>
2024-10-15 13:00:33 +0300

    Blog uygulaması yapıldı yayınlanacak

---
+++ src/components/Post.js
@@ -0,0 +1,19 @@
+export default function Post() {
+    return (
+        <div className='post'>
+            <div className='image'>
+                <img src='https://techcrunch.com/wp-content/uploads/2024/06/Apple-Vision-Pro-global-availability-e1718041644616.jpg?resize=1095,617' alt='' />
+            </div>
+            <div className="texts">
+                <h2>Apple might release a $2,000 Vision headset next year</h2>
+                <p className='info'>
+                    <span className='author'>Dawid Paszko</span>
+                    <time>2024-10-14 16:45</time>
+                </p>
+                <p className='summary'>
+                    Apple’s Vision Pro hasn’t exactly reshaped the market, but the company isn’t giving up on headsets that combine the digital and real worlds.
+                </p>
+            </div>
+        </div>
+    )
+}
